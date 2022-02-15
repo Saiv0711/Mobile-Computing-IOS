01696d84c3723eaba80c82a8d6ce87c17c464a58
Saikrishna Vuppala <S545040@nwmissouri.edu>
2022-02-14 18:23:02 -0600

    calculator

---
+++ Vuppala_Calculator/Vuppala_Calculator/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  Vuppala_Calculator
+//
+//  Created by Vuppala,Sai Krishna on 2/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var DisplayLabel: UIStackView!
+    
+    
+    @IBAction func ButtonAC(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func ButtonC(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func ChangeSign(_ sender: UIButton) {
+    }
+    
+    @IBAction func ButtonDivison(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button7(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button8(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button9(_ sender: UIButton) {
+    }
+    
+    @IBAction func ButtonMultiplicatioon(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button4(_ sender: Any) {
+    }
+    
+    @IBAction func Button5(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button6(_ sender: UIButton) {
+    }
+    
+    @IBAction func ButtonSubtraction(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button1(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button2(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button3(_ sender: UIButton) {
+    }
+    
+    @IBAction func ButtonAddition(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button0(_ sender: UIButton) {
+    }
+    
+    @IBAction func ButtonDot(_ sender: UIButton) {
+    }
+    
+    @IBAction func ButtonPercentage(_ sender: UIButton) {
+    }
+    
+    @IBAction func ButtonEqual(_ sender: UIButton) {
+    }
+    
+    
+    
+}
+
