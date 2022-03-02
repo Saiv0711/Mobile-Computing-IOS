337fc520099a90025b7ee4d58387aa824fd4b867
Saikrishna Vuppala <S545040@nwmissouri.edu>
2022-03-02 15:54:51 -0600

    Vuppala_SearchApp

---
+++ Vuppala_SearchApp/Vuppala_SearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vuppala_SearchApp
+//
+//  Created by Vuppala,Sai Krishna on 3/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
