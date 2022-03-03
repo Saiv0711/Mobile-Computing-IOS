c4437f73c8ff56fb3c3f4739abd1cc90532b5f0a
Saikrishna Vuppala <S545040@nwmissouri.edu>
2022-03-03 14:55:35 -0600

    assignment4-1

--- Vuppala_SearchApp/Vuppala_SearchApp/ViewController.swift
+++ Vuppala_SearchApp/Vuppala_SearchApp/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var search: UIButton!
+    
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    @IBOutlet weak var topicInfoText: UITextView!
+    @IBOutlet weak var prevImage: UIButton!
+    
+    @IBOutlet weak var nextImage: UIButton!
+    
+    @IBOutlet weak var resetbutton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchTextField(_ sender: UITextField) {
+    }
+    
+    @IBAction func searchButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func showNextImagesBtn(_ sender: UIButton) {
+    }
+    @IBAction func showPrevImageBtn(_ sender: UIButton) {
+    }
+    @IBAction func resetButtonClicked(_ sender: UIButton) {
+    }
 }
 
