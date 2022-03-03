9762cdc77f722014dfb668f561bfa7b4234c8b9d
Saikrishna Vuppala <S545040@nwmissouri.edu>
2022-03-03 16:13:19 -0600

    a04

---
+++ Vuppala_SearchApp/Vuppala_SearchApp/Vuppala_SearchAppUITests/Vuppala_SearchAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Vuppala_SearchAppUITestsLaunchTests.swift
+//  Vuppala_SearchAppUITests
+//
+//  Created by Vuppala,Sai Krishna on 3/2/22.
+//
+
+import XCTest
+
+class Vuppala_SearchAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
